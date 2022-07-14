c25435eb8c5686af109a9d37d54d9defb55f563f
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-14 11:15:02 +0300

    Reorganize folders and files.

---
+++ practice/assignment_operator.rb
@@ -0,0 +1,3 @@
+x, y =3, 9
+x, y = y, x
+puts "x is #{x} and y is #{y}"
---
+++ practice/to_float.rb
@@ -0,0 +1,13 @@
+puts "123.50".to_f
+puts Float("123.50")
+puts "something".to_f
+# puts Float("something")
+puts 123.5.to_s
+puts String(123.5)
+puts "123.50".to_i
+# puts Integer("123.50")
+puts 1/2
+puts 1.0/2
+puts 1.to_f/2
+puts 1/Float(2)
+puts 1.fdiv 2
