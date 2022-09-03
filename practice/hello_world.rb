016d64455cbb6bb0ca84d1d9e08b68e4e27d7421
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-09-03 13:11:58 +0300

    More practice on data types and user inputs.

--- practice/hello_world.rb
+++ practice/hello_world.rb
@@ -1,3 +1,49 @@
-require "tk"
-TkRoot.new{ title "hello World"}
-Tk.mainloop
+# character_name = 'John'
+# character_age = '35'
+
+# puts ('There was once a man named ' + character_name)
+# puts ('He was ' + character_age + ' years old')
+
+# name = 'Gideon Ng\'etich'
+# age = 28
+# floating_number = 5.9
+# is_male = true
+
+# title = nil
+
+# puts(name)
+
+# distance = 0.1
+
+# time = 9.87 / 3600
+# speed = distance / time
+
+# puts('The average speed of a sprinter is ' + speed.to_s + ' km/h.')
+
+
+# if true
+#     puts('It is True!')
+# else
+#     puts('It is False!')
+# end
+
+# if nil 
+#     puts('nil is True!')
+# else  
+#     puts('nil is False!')
+# end  
+
+# if 0
+#     puts('0 is True!')
+# else 
+#     puts('0 is False!')
+# end 
+
+# puts "String Data Type"
+# puts 'escape using "\\"'
+# puts 'That\'s right'
+
+
+puts "Enter Your Name:"
+name = gets.chomp()
+puts ("Hello " + name + ", you are cool.")
