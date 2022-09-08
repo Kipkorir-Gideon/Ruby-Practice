a351199fb5b4ae2ff99b752e984d3bf6956d49a2
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-09-08 14:17:23 +0300

    More practise on classes.

--- practice/calculator.rb
+++ practice/calculator.rb
@@ -214,3 +214,64 @@
 
 
 
+
+# class Book
+#     attr_accessor :title, :author, :pages
+
+#     def initialize(title, author, pages)
+#         @title = title
+#         @author = author
+#         @pages = pages
+#     end
+
+#     def more_than_400_pages
+#         if @pages > 400
+#             return true  
+#         end  
+#         return false
+#     end
+
+# end
+
+# book1 = Book.new("Harry Potter", "JK Rowling", 400)
+# book2 = Book.new("Lord of the Rings", "Tolkein", 453)
+
+
+# puts book2.more_than_400_pages
+
+
+
+
+class Question  
+    attr_reader :prompt, :answer  
+    def initialize(prompt, answer)
+        @prompt = prompt
+        @answer = answer
+    end  
+end  
+
+p1 = "What color are apples?\n(a)purple\n(b)green\n(c)yellow"
+p2 = "What color are bananas?\n(a)red\n(b)green\n(c)yellow"
+p3 = "What color are pears?\n(a)red\n(b)green\n(c)yellow"
+
+questions = [
+    Question.new(p1, "a"),
+    Question.new(p2, "c"),
+    Question.new(p3, "b")
+]
+
+
+def run_test(questions)
+    answer = ""
+    score = 0
+    for question in questions
+        puts question.prompt
+        answer = gets.chomp()
+        if answer == question.answer
+            score += 1
+        end
+    end
+    puts("You got " + score.to_s + "/" + questions.length().to_s)
+end
+
+run_test(questions)
